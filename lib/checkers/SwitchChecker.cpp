6071d2808bffab71f971dd726bf12aa4a223ef9a
wuyuxin <876465382@qq.com>
2020-05-15 18:41:05 +0800

    基本实现switch枚举类型覆盖不完全缺陷的检测

--- lib/checkers/SwitchChecker.cpp
+++ lib/checkers/SwitchChecker.cpp
@@ -23,22 +23,67 @@ static inline void printStmt(const Stmt* stmt, const SourceManager &sm){
 
 class SwitchVisitor : public RecursiveASTVisitor<SwitchVisitor> {
 public:
-  SwitchVisitor(const ASTContext &ctx) : ctx(ctx) {}
+  SwitchVisitor(const ASTContext &ctx,const std::vector<EnumDecl*> eds) : ctx(ctx),EDs(eds) {}
 
   bool VisitSwitchStmt(SwitchStmt *E) {
     // printStmt(E, ctx.getSourceManager());
-    Expr* conditionVar = E->getCond();
-    if(conditionVar!=NULL){
-      // printStmt(conditionVar, ctx.getSourceManager());
-      auto cond_decl = conditionVar->getReferencedDeclOfCallee();
-      // cond_decl->dump();
-      VarDecl* dec = (VarDecl*)(cond_decl);
-      // if(val==NULL)
-      //   cout<<"val is null"<<endl;
-      // else{
-      //   val->dump();
-      // }
-      auto cases = E->getSwitchCaseList();
+    Expr* conditionExpr = E->getCond();
+    // for(EnumDecl* ED:EDs){
+    //   ED->dump();
+    // }
+
+    if(conditionExpr!=NULL){
+      Decl* cond_decl = conditionExpr->getReferencedDeclOfCallee();
+      TypeDecl* TD = (TypeDecl*)(cond_decl);
+      const clang::Type* type = TD->getTypeForDecl();   //获取枚举变量定义中的类型信息
+      if(type->isEnumeralType()==false)
+        return true;      //如果该switch语句的条件不是枚举类型，则直接返回
+
+      TagType* tag = (TagType*)type;
+      string conditionEnumName = tag->getDecl()->getNameAsString();   //获取枚举变量定义中枚举类型的名称
+       
+      for(EnumDecl* ED:EDs){
+        string EnumName = ED->getNameAsString();
+
+        //判断当前switch的条件枚举变量匹配所有枚举类型声明中的哪个
+        if(conditionEnumName==EnumName){
+          //获取枚举定义中所有元素，存放进enumElements
+          std::vector<string> enumElements;
+          auto enums = ED->enumerators();
+          for(auto iter:enums){
+            enumElements.push_back(iter->getNameAsString());
+          }
+          //遍历每个case
+          SwitchCase* c = E->getSwitchCaseList();
+          while(c!=NULL){
+            auto substmts = c->child_begin()->child_begin()->children();
+            //遍历当前case子stmt，找到当前case的expr进行处理
+            for(auto substmt: substmts){
+              if(DeclRefExpr* case_expr = static_cast<DeclRefExpr*>(substmt)){
+                string caseName = case_expr->getDecl()->getNameAsString();
+                for(auto i=enumElements.begin();i!=enumElements.end();i++){
+                  if(*i==caseName){
+                    enumElements.erase(i);
+                    break;
+                  }
+                }
+                break;
+              } 
+            }
+            c=c->getNextSwitchCase();
+          }
+          //若enumElements非空，则说明有枚举情况尚未覆盖
+          if(enumElements.size()!=0){
+            cout<<E->getBeginLoc().printToString(ctx.getSourceManager())<<endl;
+            cout<<"存在尚未覆盖的枚举类型"<<endl;
+            for(auto i:enumElements){
+              cout<<i<<" ";
+            }
+            cout<<endl;
+          } 
+        }
+      }
+      
       
       // cout<<conditionVar->getType().getAsString()<<endl;
       // auto child = conditionVar->child_begin();
@@ -54,42 +99,18 @@ public:
 
 private:
   const ASTContext &ctx;
+  const std::vector<EnumDecl*> EDs;
 };
-
-// bool SwitchChecker::RecursiveFind(const Stmt* stmt, const ASTContext& context){
-//   if(stmt != nullptr){
-//     if(dyn_cast<BinaryOperator>(stmt) && ((BinaryOperator*)stmt)->isComparisonOp()){
-//       BinaryOperator* bop = (BinaryOperator*)stmt;
-//       Expr* LHS = bop->getLHS()->IgnoreImpCasts();
-//       Expr* RHS = bop->getRHS()->IgnoreImpCasts();
-//       string Ltype = LHS->getType().getAsString();
-//       string Rtype = RHS->getType().getAsString();
-//       if((find(Signed.begin(), Signed.end(), Ltype) != Signed.end() && find(Unsigned.begin(), Unsigned.end(), Rtype) != Unsigned.end())
-//       || (find(Signed.begin(), Signed.end(), Rtype) != Signed.end() && find(Unsigned.begin(), Unsigned.end(), Ltype) != Unsigned.end())){
-//         printStmt(stmt, context.getSourceManager());
-//         return true;
-//       }
-//     }else{
-//       for(auto c : stmt->children()){
-//         if(RecursiveFind(c, context)) break;
-//       }
-//     }
-//   }
-//   return false;
-// }
 
 vector<Defect> SwitchChecker::check(){
   std::vector<ASTFunction *> topLevelFuncs = call_graph->getTopLevelFunctions();
   std::vector<EnumDecl*> topLevelEnums = resource->getEnums();
-  for(EnumDecl* ED:topLevelEnums){
-    ED->dump();
-  }
   for (auto fun : topLevelFuncs) {
     const FunctionDecl *funDecl = manager->getFunctionDecl(fun);
     auto stmt = funDecl->getBody();
     const ASTContext &ctx = funDecl->getASTContext();
-    stmt->dumpColor();
-    SwitchVisitor visitor(ctx);
+    // stmt->dumpColor();
+    SwitchVisitor visitor(ctx,topLevelEnums);
     visitor.TraverseStmt(stmt);
   }
   return vector<Defect>();
