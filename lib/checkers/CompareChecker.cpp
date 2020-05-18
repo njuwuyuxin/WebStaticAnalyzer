a0bdbd28f04d3a7d21e4d3203493475e1f67514a
stnanzhao <914635416@qq.com>
2020-05-17 22:02:10 -0700

    comchecker_defectInfo

--- lib/checkers/CompareChecker.cpp
+++ lib/checkers/CompareChecker.cpp
@@ -1,5 +1,30 @@
 #include "checkers/CompareChecker.h"
 
+namespace{
+  class CompareVisitor : public RecursiveASTVisitor<CompareVisitor>{
+  public:
+    bool VisitBinaryOperator(BinaryOperator* bop){
+      if(bop->isComparisonOp()){
+        string Ltype = bop->getLHS()->IgnoreImpCasts()->getType().getAsString();
+        string Rtype = bop->getRHS()->IgnoreImpCasts()->getType().getAsString();
+        if((find(Signed.begin(), Signed.end(), Ltype) != Signed.end() && find(Unsigned.begin(), Unsigned.end(), Rtype) != Unsigned.end())
+        || (find(Signed.begin(), Signed.end(), Rtype) != Signed.end() && find(Unsigned.begin(), Unsigned.end(), Ltype) != Unsigned.end()))
+          stmts.push_back(bop);
+      }
+      return true;
+    }
+
+    vector<Stmt*> get_stmt() { return stmts; }
+  private:
+    static vector<string> Signed;
+    static vector<string> Unsigned;
+    vector<Stmt*> stmts;
+  };
+
+  vector<string> CompareVisitor::Signed = {"short", "int", "long", "long long"};
+  vector<string> CompareVisitor::Unsigned = {"unsigned short", "unsigned int", "unsigned long", "unsigned long long"};
+}
+
 vector<string> CompareChecker::Signed = {"short", "int", "long", "long long"};
 vector<string> CompareChecker::Unsigned = {"unsigned short", "unsigned int", "unsigned long", "unsigned long long"};
 
@@ -37,9 +62,26 @@ bool CompareChecker::RecursiveFind(const Stmt* stmt, const ASTContext& context){
 
 vector<Defect> CompareChecker::check(){
   getEntryFunc();
+  vector<Defect> defects;
   if(entryFunc != nullptr){
     FunctionDecl *funDecl = manager->getFunctionDecl(entryFunc);
-    /*
+    Stmt* stmt = funDecl->getBody();
+    const ASTContext& context = funDecl->getASTContext();
+    CompareVisitor visitor;
+    visitor.TraverseStmt(stmt);
+    vector<Stmt*> stmts = visitor.get_stmt();
+    for(auto s : stmts){
+      Defect d;
+      d.location = s->getBeginLoc().printToString(context.getSourceManager());
+      d.info = "compare statement has both signed and unsigned number";
+      defects.push_back(d);
+    }
+    //RecursiveFind(stmt, context);
+  }
+  return defects;
+}
+
+ /*
     vector<ASTVariable *> variables = entryFunc->getVariables();
     for(auto v: variables){
       VarDecl* varDecl = manager->getVarDecl(v);
@@ -47,13 +89,6 @@ vector<Defect> CompareChecker::check(){
       cout << v->getName() << " " << varType.getAsString() << endl;
     }
     */
-    const Stmt* stmt = funDecl->getBody();
-    const ASTContext& context = funDecl->getASTContext();
-    RecursiveFind(stmt, context);
-    //stmt->dump();
-  }
-  return vector<Defect>();
-}
 
 void CompareChecker::getEntryFunc() {
   std::vector<ASTFunction *> topLevelFuncs = call_graph->getTopLevelFunctions();
