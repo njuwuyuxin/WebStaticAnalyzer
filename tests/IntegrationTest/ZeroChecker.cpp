09fa161063cfe6a9546f3edf89cad47ec1c40395
aobamoka <suzushirochan@gmail.com>
2020-06-16 23:24:23 +0800

    realize simple dataflowanalysis, and it can raise mod-or-div-by-0 errors and warnings

--- tests/IntegrationTest/ZeroChecker.cpp
+++ tests/IntegrationTest/ZeroChecker.cpp
@@ -1,9 +1,26 @@
 int testZero2(){
-	int a=1;
-	int b=2;
-	int c=a+b*2;
-	int d = ++a;
-	int e=(1+2)*3;
-	c = a+b;
+	int a=2;
+	int b=1;
+	int c=a/b;
+	int d = 3/0;
+	int e=0;
+	if(e&&a){
+		c = a%b;
+	}
+	else{
+		c=a+b;
+	}
+	switch(b){
+		case 0:{
+			e=a/b;
+		}break;
+		case 1:case 2:{
+			e=1;
+		}break;
+		default:{
+			e=3;
+		}break;
+	}
+	int f=a>b?5:-5;
 	return 0;
 }
