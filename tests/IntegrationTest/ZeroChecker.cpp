0b8a7a34b521ead1028a11b0a8fac7d94a58d424
aobamoka <suzushirochan@gmail.com>
2020-07-13 23:04:31 +0800

    complete binary and unary operators:and/or/xor/shl/shr/not/lognot and assign operators

--- tests/IntegrationTest/ZeroChecker.cpp
+++ tests/IntegrationTest/ZeroChecker.cpp
@@ -1,47 +1,10 @@
 int testZero1(){
 	int a=2;
-	int b=0;
+	int b=1;
 	int c=a/b;
-	int d = 3/0;
+	a<<=b;
+	a=~(~a);
+	c=!(!c);
 	return 0;
 }
 
-int testZero2(){
-	int a=1;
-	int b=0;
-	int c;
-	int e=0;
-	int f=1;
-	if(e&&a){
-		c = a%b;
-	}
-	else{
-		c=a+b;
-	}
-	if(f&&a){
-		f=a/b;
-	}
-	else{
-		f=a+b;
-	}
-	return 0;
-}
-
-int testZero3(){
-	int a=1;
-	int b=0;
-	int e=2;
-	switch(b){
-		case 0:{
-			e=a/b;
-		}break;
-		case 1:case 2:{
-			e=1;
-		}break;
-		default:{
-			e=3;
-		}break;
-	}
-	a=b/e;
-	return 0;
-}
